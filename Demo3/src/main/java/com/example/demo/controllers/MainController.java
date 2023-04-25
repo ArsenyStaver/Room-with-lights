13808b3d23b99b0f3330b1ca47cd414bd6101793
ArsenyStaver <92537314+ArsenyStaver@users.noreply.github.com>
2023-04-25 13:16:23 +0200

    Add files via upload

---
+++ Demo3/src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controllers;
+
+import com.example.demo.Models.Post;
+import com.example.demo.controllers.repository.PostRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    @GetMapping("/")
+    public String test(Model model) {
+        model.addAttribute("test");
+        return "test";
+    }
+
+
+    // worked version
+    @PostMapping("/")
+    public String roomAdd(@RequestParam String name, @RequestParam("country") String country, Model model, HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-FORWARDED-FOR");
+        if (ipAddress == null) {
+            ipAddress = request.getRemoteAddr();
+        }
+        System.out.println("User IP Address: " + ipAddress);
+
+
+        Post post = new Post(name, country);
+
+        postRepository.save(post);
+
+        long id = post.getId();
+
+
+        return "redirect:/room/" + id; // Redirect the user to the room page with the specified id
+
+    }
+
+}
+
+
+
+
+//192.168.0.241:8081
---
+++ Demo3/src/main/java/com/example/demo/controllers/WebSocketConfig.java
@@ -0,0 +1,63 @@
+package com.example.demo.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+import java.io.IOException;
+
+
+import java.util.*;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/socket").setAllowedOrigins("*");
+    }
+
+
+    private WebSocketHandler myHandler() {
+        return new TextWebSocketHandler() {
+            private final Set<WebSocketSession> sessions = new HashSet<>();
+
+            @Override
+            public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+                sessions.add(session);
+            }
+
+            @Override
+            protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+                String payload = message.getPayload();
+                for (WebSocketSession s : sessions) {
+                    if (s == session) { // отправляем сообщение только обратно на текущий сеанс
+                        s.sendMessage(new TextMessage(payload));
+                        break;
+                    }
+                }
+            }
+
+
+            @Override
+            public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+                sessions.remove(session);
+            }
+        };
+    }
+
+
+}
+
+
+
+
+
+
